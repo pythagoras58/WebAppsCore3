5245f2da19a799147b731aab8e568c36a480f56f
pythagoras58 <solomondanso58@gmail.com>
2023-01-27 21:53:53 +0000

    Data Access

--- WebAppsCore3/Models/Command.cs
+++ WebAppsCore3/Models/Command.cs
@@ -5,5 +5,9 @@
         public int Id { get; set; }
 
         public string HowTo { get; set; }
+
+        public string Line { get; set; }
+
+        public string Platform { get; set; }
     }
 }
