5192c135a31370e477a2e4f8279d112d23bc4166
pythagoras58 <solomondanso58@gmail.com>
2023-01-27 22:03:59 +0000

    Interface Implementation

---
+++ WebAppsCore3/Data/ICommanderInterface.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebAppsCore3.Models;
+
+namespace WebAppsCore3.Data
+{
+    public interface ICommanderInterface
+    {
+        IEnumerable<Command> GetAppCommands();
+
+        Command GetCommandById(int id);
+    }
+}
---
+++ WebAppsCore3/Data/MockCommanderRepo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using WebAppsCore3.Models;
+
+namespace WebAppsCore3.Data
+{
+    public class MockCommanderRepo : ICommanderInterface
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Command GetCommandById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
