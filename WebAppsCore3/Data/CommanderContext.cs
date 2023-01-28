4c79300c37bdf188b543bfc6f8c29b3c31214f73
pythagoras58 <solomondanso58@gmail.com>
2023-01-28 13:26:10 +0000

    Db Connection

---
+++ WebAppsCore3/Data/CommanderContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppsCore3.Models;
+
+namespace WebAppsCore3.Data
+{
+    public class CommanderContext : DbContext
+    {
+        public CommanderContext(DbContextOptions<CommanderContext> opt) : base(opt) 
+        {
+                
+        }
+
+        //create a representation of the Db
+        public DbSet<Command> Commands { get; set; }    
+    }
+}
--- WebAppsCore3/Startup.cs
+++ WebAppsCore3/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,9 @@ namespace WebAppsCore3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommanderContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("CommanderConnection")));
+
             services.AddControllers();
             services.AddScoped<ICommanderRepo, MockCommanderRepo>(); // DI 
         }
