2aabb71b4097be7b97d8302d2d47d0d659031e1e
pythagoras58 <solomondanso58@gmail.com>
2023-01-28 10:25:22 +0000

    Controller 1

--- WebAppsCore3/Controllers/CommandsController.cs
+++ WebAppsCore3/Controllers/CommandsController.cs
@@ -1,7 +1,28 @@
-﻿namespace WebAppsCore3.Controllers
-{
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebAppsCore3.Data;
+using WebAppsCore3.Models;
 
-    public class CommandsController
+namespace WebAppsCore3.Controllers
+{
+    [Route("api/[controller]")]    
+    [ApiController]
+    public class CommandsController : ControllerBase
     {
+        // make an instance of the repo
+        private readonly MockCommanderRepo _commanderRepo = new MockCommanderRepo();
+
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands() {
+            //use repository to pull the data
+            return null;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            return null;
+        }
     }
 }
