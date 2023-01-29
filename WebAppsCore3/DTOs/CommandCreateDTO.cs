9b68ec05a13e83b5e8a396d827cba19e8ac5dbcf
pythagoras58 <solomondanso58@gmail.com>
2023-01-29 19:44:59 +0000

    Create DTO 1

--- WebAppsCore3/DTOs/CommandCreateDTO.cs
+++ WebAppsCore3/DTOs/CommandCreateDTO.cs
@@ -3,13 +3,9 @@
 namespace WebAppsCore3.DTOs
 {
     public class CommandCreateDTO 
-    {
-        public int Id { get; set; }
-
-   
+    {   
         public string HowTo { get; set; }
 
-   
         public string Line { get; set; }
 
         public string Platform { get; set; }
